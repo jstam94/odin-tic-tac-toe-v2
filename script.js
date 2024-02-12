895113e37cc82f572ce9f25744dc60563f38b926
Jordan Stammer <s.jordanstammer@gmail.com>
2024-02-12 13:28:23 -0500

    Player Selection Built
    Functionality built to select players, this can only be run if
    players have not already been selected

--- script.js
+++ script.js
@@ -10,9 +10,49 @@ console.log('Script Connected');
 
 
 
+// Module to expose only needed game inputs
+
+gameController = (function(){
+    let currentPlayer;
+    let gameOver = false;
+    
+let players = (function(){
+    let players;
+    let getPlayers = () => players;
+    let createPlayer = function (name, mark){
+        return {name, mark}
+    }
+    let selection = function (){
+        if (!players){
+        let player1 = createPlayer( document.getElementById('player1').value, 'x')
+        let player2 = createPlayer( document.getElementById('player2').value, 'o')
+        players = [player1, player2]
+        } else return;
+    }
+    return {selection, getPlayers}
+})();
+
+
+start = function(){
+ 
+}
+
+restart = function(){
+
+}
+
+playRound = function(position, player){
+
+}
+
+// {start, restart, playRound} should be only things exposed at end, will temporarily expose other things to test
+return {start, restart, playRound, players}
+}) ()
 
 
 // Module that will controll gameController via Event Listeners
 screenController = (function(){
+
+
 
 })();
