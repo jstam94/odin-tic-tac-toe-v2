cbb7a466b956f0baa76041c34480bf946a5aa3c2
Jordan Stammer <s.jordanstammer@gmail.com>
2024-02-13 10:16:14 -0500

    Win Condition added
    Game will now move to GameOverState after win condition has been filled

--- script.js
+++ script.js
@@ -1,94 +1,117 @@
-
-// Module to expose only needed game inputs
-
 let gameController = (function(){  
-let currentPlayer 
-let gameStarted = false;
-let gameOver = false;
 
-printStatus = function (){
-    console.log({currentPlayer, gameStarted, gameOver})
-}
+    let currentPlayer 
+    let gameStarted = false;
+    let gameOver = false;
+
+    printStatus = function (){
+        console.log({currentPlayer, gameStarted, gameOver})
+    }
     
-let players = (function(){
-    let players;
-    let getPlayers = () => players;
-    let createPlayer = function (name, mark){
-        return {name, mark}
-    }
-    let selection = function (){
-        if (!players){
-        let player1 = createPlayer( document.getElementById('player1').value, 'x')
-        let player2 = createPlayer( document.getElementById('player2').value, 'o')
-        players = [player1, player2];
-        } else return;
-    }
-    let clear = function () {
-        players = undefined;
-    }
-    return {selection, getPlayers, clear}
-})();
+    let players = (function(){
+        let players;
+        let getPlayers = () => players;
+        let createPlayer = function (name, mark){
+            return {name, mark}
+        }
+        let selection = function (){
+            if (!players){
+            let player1 = createPlayer( document.getElementById('player1').value, 'x')
+            let player2 = createPlayer( document.getElementById('player2').value, 'o')
+            players = [player1, player2];
+            } else return;
+        }
+        let clear = function () {
+            players = undefined;
+        }
+        return {selection, getPlayers, clear}
+    })();
 
-let board = (function(){
-    let board = [null, null, null, null, null, null, null, null, null,]
-    let get = () => board;
-    let clear = function (){
-        board.forEach(cell => {
-            cell = null;
+    let board = (function(){
+        let board = [null, null, null, null, null, null, null, null, null,]
+        let get = () => board;
+        let clear = function (){
+            board.forEach(cell => {
+                cell = null;
+            });
+        }
+        let select = function(cell, mark){
+            if (!(board[cell])){
+            board[cell] = mark;
+            checkForWin();
+            (currentPlayer === players.getPlayers()[0]) ? currentPlayer = players.getPlayers()[1] : currentPlayer = players.getPlayers()[0];
+            } else return;
+        }
+
+        return {clear, select, get}
+    })()
+
+
+    function start (){
+        if (gameOver || gameStarted) return;
+        gameStarted = true;
+        players.selection();
+        currentPlayer = players.getPlayers()[0];
+    }
+
+    restart = function(){
+
+    }
+
+    function win(){
+        console.log('winner found')
+        gameOver = true;
+    }
+
+    let checkForWin = function (){
+        console.log(`Checking if ${currentPlayer.name} is the winner`)
+        let winningCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+        ]
+        
+        winningCombos.forEach(combo => {
+            if (board.get()[combo[0]] === currentPlayer.mark && board.get()[combo[1]] == currentPlayer.mark && board.get()[combo[2]] == currentPlayer.mark){
+                win();
+            }
         });
     }
-    let select = function(cell, mark){
-        if (!(board[cell])){
-        board[cell] = mark;
-        (currentPlayer === players.getPlayers()[0]) ? currentPlayer = players.getPlayers()[1] : currentPlayer = players.getPlayers()[0];
-        } else return;
+
+    let playRound = function(cell){
+        if (gameOver || !gameStarted) return;
+        board.select(cell, currentPlayer.mark);
     }
 
-    return {clear, select, get}
-})()
 
-
- function start (){
-    if (gameOver || gameStarted) return;
-    gameStarted = true;
-    players.selection();
-    currentPlayer = players.getPlayers()[0];
-}
-
-restart = function(){
-
-}
-
-// let checkWinCondition = function (){
-// console.log('running checkWinCondition')
-// }
-
-let playRound = function(cell){
-    if (gameOver || !gameStarted) return;
-    console.log(cell, currentPlayer.mark)
-    board.select(cell, currentPlayer.mark);
-    // checkWinCondition();
-}
-
-
-// {start, restart, playRound} should be only things exposed at end, will temporarily expose other things to test
-return {start, restart, playRound, board, printStatus, players}
+return {start, restart, playRound, board, printStatus}
 }) ()
 
 
-// Module that will controll gameController via Event Listeners
+
 
 screenController = (function(){
-   
+    let cells = document.querySelectorAll('.cell');
+
+    function render(){
+        cells.forEach(cell => {
+            cell.textContent = gameController.board.get()[+cell.getAttribute('id')]
+        });
+    }
      document.getElementById('start').addEventListener('click', () => gameController.start());
     
-    let cells = document.querySelectorAll('.cell');
+    
 
 
     cells.forEach(cell => {
         cell.addEventListener('click', () => {
-            console.log('click');
-            gameController.playRound(+cell.getAttribute('id'))
+            gameController.playRound(+cell.getAttribute('id'));
+            render();
         })
     });
 
